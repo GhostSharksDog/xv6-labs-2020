a15e4942085e38c44d87df527737483de89a8956
GhostSharksDog <1766505342@qq.com>
2025-11-03 14:27:33 +0800

    Lab3_2

--- kernel/defs.h
+++ kernel/defs.h
@@ -160,8 +160,8 @@ int             uartgetc(void);
 // vm.c
 void            kvminit(void);
 void            kvminithart(void);
-uint64          kvmpa(uint64);
-void            kvmmap(uint64, uint64, uint64, int);
+uint64          kvmpa(pagetable_t pgtbl, uint64 va);
+void            kvmmap(pagetable_t pgtbl, uint64 va, uint64 pa, uint64 sz, int perm);
 int             mappages(pagetable_t, uint64, uint64, uint64, int);
 pagetable_t     uvmcreate(void);
 void            uvminit(pagetable_t, uchar *, uint);
@@ -180,6 +180,9 @@ int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
 //Lab3_1
 void            vmprint(pagetable_t);
+void            kvm_free_kernelpgtbl(pagetable_t pagetable);
+pagetable_t     kvminit_newpgtbl(void);
+void            kvm_map_pagetable(pagetable_t pgtbl);
 
 // plic.c
 void            plicinit(void);
